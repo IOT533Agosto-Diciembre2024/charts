9083dba570da76c80e68744defd0195e958d1bb6
Andrés Torres <atorres@applab.mx>
2024-11-13 10:37:37 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
+interface Sensores {
+  id: number;
+  temperatura: number;
+  ax: number;
+  ay: number;
+  presion: number;
+  momento: string;
+}
 function App() {
+  const [sensores, setSensores] = React.useState<Sensores[]>([]);
+
+  async function loadSensores() {
+    const sensores = await axios.get("http://localhost:8000/sensores");
+    setSensores(sensores.data);
+  }
+
+  useEffect(() => {
+    loadSensores();
+    const interval = setInterval(() => {
+      loadSensores();
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Hola</h1>
+      <ResponsiveContainer width={1000} height={500}>
+        <LineChart
+          width={500}
+          height={300}
+          data={sensores}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="momento" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="temperatura" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
